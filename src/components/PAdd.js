5bd73ed6e913b134ce07853d05d608c314263d40
005689 <417214710@qq.com>
2017-03-01 09:12:01 +0800

    添加候补旅客页面

---
+++ src/components/PAdd.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import * as F from '../utils/Func'
+import * as C from '../utils/Const'
+import {
+  Form, Select, InputNumber, Switch, Radio,
+  Slider, Button, Upload, Icon, Input, Row, Col
+} from 'antd';
+const FormItem = Form.Item;
+const Option = Select.Option;
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+
+export default class PAdd extends React.Component {
+
+  handleSubmit() {
+
+  }
+
+  render() {
+    const {dss, currActive, currBlock} = this.props
+
+    const isCurrPage = currBlock === C.PAGE_ADD_PASSENGER
+    let dsField;
+    if (dss.length == 1) {
+      dsField = <RadioGroup>
+        <Radio value={dsField[0]} checked>{dsField[0]}</Radio>
+      </RadioGroup>
+    } else {
+      dsField = <RadioGroup>
+        {
+          dss.map((ds, i)=> {
+            const k = `key-ds-${i}`
+            const isActive = currActive == k
+            if (isActive) {
+              return <Radio value={ds} checked className={isCurrPage && F.getActiveCls(isActive)}>{ds}</Radio>
+            } else {
+              return <Radio value={ds}>{ds}</Radio>
+            }
+          })
+        }
+      </RadioGroup>
+    }
+
+    const formItemLayout = {
+      labelCol: {span: 6},
+      wrapperCol: {span: 14},
+    };
+
+    return <div>
+      <Row>
+        <Col span={24} className="add-passenger-header">
+          候补旅客
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <Form onSubmit={this.handleSubmit}>
+            <FormItem
+              {...formItemLayout}
+              label="目的站"
+            >
+              {dsField}
+            </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label="旅客姓名"
+              validateStatus="error"
+              help="Should be combination of numbers & alphabets"
+            >
+              <Input className={F.getActiveCls(isCurrPage && currActive == C.PADD_CN_KEY)}
+                     placeholder="unavailable choice"
+                     id="error"/>
+            </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label="旅客性质"
+            >
+              <RadioGroup>
+                <Radio value="M" className={F.getActiveCls(isCurrPage && currActive == C.PADD_SEX1_KEY)}>M</Radio>
+                <Radio value="C" className={F.getActiveCls(isCurrPage && currActive == C.PADD_SEX2_KEY)}>C</Radio>
+                <Radio value="I" className={F.getActiveCls(isCurrPage && currActive == C.PADD_SEX3_KEY)}>I</Radio>
+              </RadioGroup>
+            </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label="证件号码"
+              validateStatus="error"
+              help="Should be combination of numbers & alphabets"
+
+            >
+              <Input className={F.getActiveCls(isCurrPage && currActive == C.PADD_DN_KEY)}
+                     placeholder="unavailable choice"
+                     id="error"/>
+            </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label="电子票"
+              validateStatus="error"
+              help="Should be combination of numbers & alphabets"
+            >
+              <Input className={F.getActiveCls(isCurrPage && currActive == C.PADD_ETI_KEY)}
+                     placeholder="unavailable choice"
+                     id="error"/>
+            </FormItem>
+
+
+            <FormItem wrapperCol={{span: 16, offset: 6}}>
+              <Button className={F.getActiveCls(isCurrPage && currActive == C.SUBMIT_BTN_KEY)} type="primary"
+                      htmlType="submit">提交</Button>
+            </FormItem>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  }
+}
---
+++ src/components/PList.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import {Table} from 'antd'
+import PDetail from '../components/PDetail'
+import * as F from '../utils/Func'
+
+export default class PList extends React.Component {
+
+  render() {
+
+    const {pls, selectPls, currActive, onRowSelect, onRowSelectAll, onRowClick} = this.props
+
+    const columns = [
+      {
+        title: '状态',
+        key: 'status',
+        width: 100,
+        render: (r) => {
+          const isDel = r.scf && $.trim(r.scf.toLocaleUpperCase()) === 'DEL'
+          const isAc = r.wci
+          return <span>
+            {isDel ? <span className="dcs-squar">退</span> : null}
+            <span className="dcs-squar">{isAc ? 'AC' : 'NA'}</span>
+          </span>
+        },
+      },
+      {title: '姓名', dataIndex: 'cn', key: '1', width: 150},
+      {title: '性质', dataIndex: 'sex', key: '2', width: 150},
+      {title: '订单号', dataIndex: 'orn', key: '3', width: 150},
+      {title: '座位', dataIndex: 'sea', key: '4', width: 150},
+      {title: '目的地', dataIndex: 'ds', key: '5', width: 150},
+      {title: '免额行李', dataIndex: 'fbw', key: '6', width: 150},
+      {title: '行李重量', dataIndex: 'tbw', key: '7', width: 150},
+      {title: '服务', dataIndex: 'osi', key: '8'},
+    ];
+
+    const rowSelection = {
+      selectedRowKeys: selectPls.map((pl)=>F.genPlKey(pl)),
+      onChange: (selectedRowKeys, selectedRows) => {
+      },
+      onSelect: (record, selected, selectedRows) => {
+        if (typeof onRowSelect === 'function') {
+          onRowSelect(selected, record)
+        }
+      },
+      onSelectAll: (selected, selectedRows, changeRows) => {
+        if (typeof onRowSelectAll === 'function') {
+          onRowSelectAll(selected)
+        }
+      },
+    }
+
+    return <Table className="dcs-pl-table"
+                  title={(o)=> <span><span className="dcs-circle">i</span>共<span
+                    className="dcs-pl-table-num">{pls.length}</span>条旅客数据，已选择<span
+                    className="dcs-pl-table-num">{selectPls.length}</span>条</span>}
+                  rowKey={pl=>F.genPlKey(pl)}
+                  rowSelection={rowSelection}
+                  columns={columns}
+                  pagination={false}
+                  dataSource={pls}
+                  onRowClick={(record)=> {
+
+                    const isSelected = selectPls.some(pl=>pl.uui == record.uui)
+                    if (typeof onRowClick === 'function') {
+                      onRowClick(isSelected, record)
+                    }
+                  }}
+                  expandedRowKeys={[currActive || '']}
+                  expandedRowRender={r=> {
+                    const pDetailProps = {
+                      pl: r
+                    }
+                    return <PDetail {...pDetailProps} />
+                  }}
+                  rowClassName={(r)=> {
+                    return F.getActiveCls(currActive == F.genPlKey(r))
+                  }}
+                  scroll={{y: 400}}/>
+  }
+}
